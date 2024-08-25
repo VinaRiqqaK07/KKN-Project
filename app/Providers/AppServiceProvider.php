2437afe4666956a9d75fa234d22ba91c6021c4e9
Vina Riqqa Khairiah <120655559+VinaRiqqaK07@users.noreply.github.com>
2024-08-25 08:27:27 +0800

    Update AppServiceProvider.php if null

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,30 +24,15 @@ class AppServiceProvider extends ServiceProvider
         //
         View::composer('components.layouts.app', function ($view) {
             $logos = MediaContent::where('type', 'logo')->first();
-            //dd($logos);
-            $url = $logos->getImageUrl();
-            if ($url) {
-                $logos->imageUrl = Str::replaceFirst(config('app.url') . '/storage', 'storage', $url);
-            } else {
-                $logos->imageUrl = null;
+            if ($logos) {
+                $url = $logos->getImageUrl();
+                if ($url) {
+                    $logos->imageUrl = Str::replaceFirst(config('app.url') . '/storage', 'storage', $url);
+                } else {
+                    $logos->imageUrl = null;
+                }
             }
-            // $logos->each(function ($logo) {
-            //     $url = $logo->getImageUrl();
-            //     //dd($url);
-            //     if ($url) {
-            //         $logo->imageUrl = Str::replaceFirst(config('app.url') . '/storage', 'storage', $url);
-            //     } else {
-            //         $logo->imageUrl = null;
-            //     }
-            //     //dd($logo);
-                
-            // });
-            // dd($logos);
-            // $logos->each(function ($logo) {
-            //     dd($logo->imageUrl);
-            // });
-            //dd($logos->imageUrl);
-            //dd(count($logos));
+            
             $view->with('logos', $logos);
         });
     }
